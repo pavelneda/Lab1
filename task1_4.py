4d7513f1f43445fb686c3ebe76f4eb17faca4a24
pavelneda <90754825+pavelneda@users.noreply.github.com>
2021-09-16 13:54:21 +0300

    Update task1_4.py

--- task1_4.py
+++ task1_4.py
@@ -5,6 +5,7 @@ def maxgold(W, wgt):
     for i in range(n + 1): 
         for j in range(W + 1): 
             if i == 0 or j == 0: 
+            if not i or not j: 
                 tab[i][j] = 0
             elif wgt[i-1] <= j: 
                 tab[i][j] = max(wgt[i-1] + tab[i-1][j-wgt[i-1]],  tab[i-1][j]) 
