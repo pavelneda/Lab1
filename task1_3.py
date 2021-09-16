2d02250c7304205a6f7ef8a315581bdf68a5abbf
pavelneda <90754825+pavelneda@users.noreply.github.com>
2021-09-16 13:54:04 +0300

    Update task1_3.py

--- task1_3.py
+++ task1_3.py
@@ -6,7 +6,6 @@ def filtr(l,i=0):
     if l[i] == "\0" :
         print("(True,",str(eval(st))+")")
     else:
-        if l[i].isdigit() or (((l[i] == "+") or (l[i] == "-"))  and (l[i+1].isdigit() or (l[i+1] == "\0"))):
             i=i+1
             filtr(l,i)   
         else:
